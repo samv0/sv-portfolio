2e93ecaa97e204fa53c46e2cbf17fc6b214af132
Sam Vinh <samvinh0x@gmail.com>
2023-02-07 10:14:02 -0800

    Complete contact section

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Footer.scss'
-
+import { images } from '../../constants';
 
 const Footer = () => {
   return (
-    <div></div>
+    <div id='contact'>
+      <h2 className="head-text">Reach out to me at</h2>
+
+      <div className="app__footer-cards">
+        <div className="app__footer-card ">
+          <img src={images.email} alt="email" />
+          <a href="mailto:contact@samvinh.com" className="p-text">contact@samvinh.com</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
